edebf6337d839c2ea3cc9d7ec34e348720c00370
Fahim Shahriar Nur <shahriarfahim091@gmail.com>
2026-08-19 10:57:57 +0600

    Fix production CORS

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ const PORT = process.env.PORT || 8009;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://YOUR-VERCEL-DOMAIN.vercel.app"],
     credentials: true,
   }),
 );
